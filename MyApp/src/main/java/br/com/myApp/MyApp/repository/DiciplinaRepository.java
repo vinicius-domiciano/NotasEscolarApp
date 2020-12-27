d0dc76e8f59bc7c4859c7d8888fa9ed03f22b966
Vinicius Domiciano <ViniciusAlexDomiciano@gmail.com>
2020-12-27 12:25:05 -0300

    Adicionando repository das novas entidades

---
+++ MyApp/src/main/java/br/com/myApp/MyApp/repository/DiciplinaRepository.java
@@ -0,0 +1,7 @@
+package br.com.myApp.MyApp.repository;
+
+import br.com.myApp.MyApp.model.Diciplina;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiciplinaRepository extends JpaRepository<Diciplina, Long> {
+}
---
+++ MyApp/src/main/java/br/com/myApp/MyApp/repository/TurmaRepository.java
@@ -0,0 +1,7 @@
+package br.com.myApp.MyApp.repository;
+
+import br.com.myApp.MyApp.model.Turma;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TurmaRepository extends JpaRepository<Turma, Long> {
+}
