55e3a44f08c64f4da854ff5126346ea49fc2735f
Vinicius D. Alexandrino <viniciusalexdomiciano@gmail.com>
2021-02-01 21:18:57 -0300

    Adicionando metodo para retorno de banco e adicionando novas rotas

--- src/main/java/br/com/myApp/MyApp/repository/NotasRepository.java
+++ src/main/java/br/com/myApp/MyApp/repository/NotasRepository.java
@@ -1,11 +1,16 @@
 package br.com.myApp.MyApp.repository;
 
+import br.com.myApp.MyApp.model.Aluno;
+import br.com.myApp.MyApp.model.enumerations.BimestreEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.myApp.MyApp.model.Notas;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface NotasRepository extends JpaRepository<Notas, UUID> {
+
+    Optional<Notas> findByAnoAndBimestreAndAluno(int ano, BimestreEnum bimestre, Aluno aluno);
 
 }
