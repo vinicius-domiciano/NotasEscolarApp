48c85a017cf3c8185391b7a8d63d0668e3241e75
Vinicius D. Alexandrino <viniciusalexdomiciano@gmail.com>
2021-02-01 21:19:51 -0300

    Adicionando query dinamica para filtros de turma

--- src/main/java/br/com/myApp/MyApp/repository/TurmaRepository.java
+++ src/main/java/br/com/myApp/MyApp/repository/TurmaRepository.java
@@ -1,9 +1,19 @@
 package br.com.myApp.MyApp.repository;
 
+import br.com.myApp.MyApp.model.Aluno;
 import br.com.myApp.MyApp.model.Turma;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface TurmaRepository extends JpaRepository<Turma, UUID> {
+
+    @Query(
+            value = "SELECT t.* FROM tbl_turma AS t WHERE periodo LIKE %?1% AND serie LIKE %?2% AND turma LIKE %?3%",
+            nativeQuery = true
+    )
+    List<Turma> filtersParams(String periodo, String serie, String turma);
+
 }
