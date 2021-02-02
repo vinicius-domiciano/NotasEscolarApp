95ce7341590a3cd310fa28b1f43d09cc231aa2c3
Vinicius D. Alexandrino <viniciusalexdomiciano@gmail.com>
2021-02-01 21:16:58 -0300

    Adicionando campos de indentificação de diciplina e notas

--- src/main/java/br/com/myApp/MyApp/model/dto/pontos/PontosDefaultDTO.java
+++ src/main/java/br/com/myApp/MyApp/model/dto/pontos/PontosDefaultDTO.java
@@ -1,6 +1,9 @@
 package br.com.myApp.MyApp.model.dto.pontos;
 
 import br.com.myApp.MyApp.model.Pontos;
+import br.com.myApp.MyApp.model.dto.diciplina.DiciplinaDefaultDTO;
+import br.com.myApp.MyApp.model.dto.notas.NotasDefaultDTO;
+import br.com.myApp.MyApp.model.dto.notas.NotasIdentifyDTO;
 
 import java.util.List;
 import java.util.UUID;
@@ -10,6 +13,8 @@ public class PontosDefaultDTO {
 
     private UUID idPonto;
     private double pontuacao;
+    private NotasIdentifyDTO notaIdentify;
+    private DiciplinaDefaultDTO diciplinaIdentify;
 
     /*Construtor*/
 
@@ -19,6 +24,8 @@ public class PontosDefaultDTO {
     public PontosDefaultDTO(Pontos pontos) {
         this.idPonto = pontos.getIdPonto();
         this.pontuacao = pontos.getPontuacao();
+        this.notaIdentify = new NotasIdentifyDTO(pontos.getNota());
+        this.diciplinaIdentify = new DiciplinaDefaultDTO(pontos.getDiciplina());
     }
 
     /** GETTER E SETTER **/
@@ -37,6 +44,22 @@ public class PontosDefaultDTO {
 
     public void setPontuacao(double pontuacao) {
         this.pontuacao = pontuacao;
+    }
+
+    public NotasIdentifyDTO getNotaIdentify() {
+        return notaIdentify;
+    }
+
+    public void setNotaIdentify(NotasIdentifyDTO notaIdentify) {
+        this.notaIdentify = notaIdentify;
+    }
+
+    public DiciplinaDefaultDTO getDiciplinaIdentify() {
+        return diciplinaIdentify;
+    }
+
+    public void setDiciplinaIdentify(DiciplinaDefaultDTO diciplinaIdentify) {
+        this.diciplinaIdentify = diciplinaIdentify;
     }
 
     /*Metodos*/
