3cc09344c777b2feafea72ad53888e30536b16f9
Vinicius D. Alexandrino <viniciusalexdomiciano@gmail.com>
2021-02-01 21:13:59 -0300

    Adicionando campo de alunoIdentify para setar uma identificação do aluno pelo [id, nome]

--- src/main/java/br/com/myApp/MyApp/model/dto/notas/NotasDefaultDTO.java
+++ src/main/java/br/com/myApp/MyApp/model/dto/notas/NotasDefaultDTO.java
@@ -1,8 +1,16 @@
 package br.com.myApp.MyApp.model.dto.notas;
 
 import br.com.myApp.MyApp.model.Notas;
+import br.com.myApp.MyApp.model.dto.aluno.AlunoDefaultDTO;
+import br.com.myApp.MyApp.model.dto.aluno.AlunoIdentifyDTO;
 import br.com.myApp.MyApp.model.enumerations.BimestreEnum;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -13,6 +21,9 @@ public class NotasDefaultDTO {
     private int ano;
     private BimestreEnum bimestre;
 
+    @NotNull(message = "É necessario enviar o aluno com seu id")
+    private AlunoIdentifyDTO alunoIdentify;
+
     //Construtor
 
     public NotasDefaultDTO() {
@@ -22,6 +33,7 @@ public class NotasDefaultDTO {
         this.idNota = notas.getIdNota();
         this.ano = notas.getAno();
         this.bimestre = notas.getBimestre();
+        this.alunoIdentify = new AlunoIdentifyDTO(notas.getAluno());
     }
 
     /** GETTER E SETTER **/
@@ -48,6 +60,14 @@ public class NotasDefaultDTO {
 
     public void setBimestre(BimestreEnum bimestre) {
         this.bimestre = bimestre;
+    }
+
+    public AlunoIdentifyDTO getAlunoIdentify() {
+        return alunoIdentify;
+    }
+
+    public void setAlunoIdentify(AlunoIdentifyDTO alunoIdentify) {
+        this.alunoIdentify = alunoIdentify;
     }
 
     /*Metodos*/
