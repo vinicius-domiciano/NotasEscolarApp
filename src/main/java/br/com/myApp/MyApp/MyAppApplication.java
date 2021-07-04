b9bceabbe0064651b19e75923b825088338e8cc2
Vinicius D. Alexandrino <viniciusalexdomiciano@gmail.com>
2021-07-04 02:28:33 -0300

    Adicionando novas anotações a classe Main

--- src/main/java/br/com/myApp/MyApp/MyAppApplication.java
+++ src/main/java/br/com/myApp/MyApp/MyAppApplication.java
@@ -1,9 +1,13 @@
 package br.com.myApp.MyApp;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
 public class MyAppApplication {
 
 	public static void main(String[] args) {
